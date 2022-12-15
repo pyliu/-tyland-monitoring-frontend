8040b1af3ce4c6354375b3a55231854a3181ab2e
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-15 16:55:04 +0800

    test

--- app.vue
+++ app.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
 div: NuxtWelcome
+div
+  h1 Welcome to the homepage
+  AppAlert This is an auto-imported component.
 </template>
 
 <script setup>
+const appConfig = useAppConfig();
 const runtimeConfig = useRuntimeConfig();
 </script>
-
---
+++ components/AppAlert.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+span: slot
+</template>
