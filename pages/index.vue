5cc0248dce7ad43613b5bdee4fbdfbf524684908
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-19 17:55:44 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,35 @@
 <script setup>
-  import { computed } from "@vue/reactivity";
+import { computed } from "@vue/reactivity";
 import { onBeforeMount, onBeforeUpdate, onMounted, onUnmounted, onUpdated } from "vue";
 
-  const  props = defineProps({
-    idx: { type: Number, default: 99 }
-  })
+const  props = defineProps({
+  idx: { type: Number, default: 99 }
+})
 
-  const emit = defineEmits([])
+const emit = defineEmits([])
 
-  // const appConfig = useAppConfig()
-  // const runtimeConfig = useRuntimeConfig()
+// const appConfig = useAppConfig()
+// const runtimeConfig = useRuntimeConfig()
 
-  const data = computed(() => {
-    return {
-      name: 'pyliu',
-      age: 45
-    }
-  })
+const data = computed(() => {
+  return {
+    name: 'pyliu',
+    age: 45
+  }
+})
 
-  onBeforeMount(()=> {})
-  onMounted(()=> {
-    console.log('mounted!!', data)
-  })
-  onBeforeUpdate(()=> {})
-  onUpdated(()=> {})
-  onUnmounted(()=> {})
+onBeforeMount(()=> {})
+onMounted(()=> {
+  console.log('mounted!!', data)
+})
+onBeforeUpdate(()=> {})
+onUpdated(()=> {})
+onUnmounted(()=> {})
 </script>
 
 <template lang="pug">
-NuxtWelcome
-<nuxt-link to="/test">test</nuxt-link>
+nuxt-welcome
+nuxt-link(to="/test") test
 </template>
 
 <style lang="scss" scoped>
